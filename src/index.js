a022ba7555efa440504ecc2a97d723609569bc92
Cristian <crisgianoboli@gmail.com>
2022-06-14 15:42:37 -0300

    alert add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Swal from "sweetalert2";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// mejorar el alert
-alert("Recuerde habilitar la ubicacion de su dispositivo");
+Swal.fire({
+  icon: "info",
+  title: "Preste atención",
+  text: "Recuerde habilitar la ubicacion de su dispositivo",
+});
 root.render(
   <React.StrictMode>
     <Home />
